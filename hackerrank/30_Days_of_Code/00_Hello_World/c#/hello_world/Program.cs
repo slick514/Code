082a927ad95e1ac0796729c9dc2a5045e3c7dc62
Justin Gries <justin.gries@gmail.com>
2020-05-20 20:53:11 -0700

    c# 'hello-world' code submitted

---
+++ hackerrank/30_Days_of_Code/00_Hello_World/c#/hello_world/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            Console.WriteLine("Hello, World.");
+            Console.WriteLine(input);
+        }
+    }
+}
