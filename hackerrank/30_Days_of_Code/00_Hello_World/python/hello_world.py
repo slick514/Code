3db2c105fbcd5a62d8c79aa00ba59ece967ba08c
Justin Gries <justin.gries@gmail.com>
2020-05-21 17:47:17 -0700

    python code for 'hello-world' works

---
+++ hackerrank/30_Days_of_Code/00_Hello_World/python/hello_world.py
@@ -0,0 +1,8 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# print the content that was read in, to stdout
+print(input_string)
