5953759650588d931bef690f77f5c3c3c0078ab3
Justin Gries (Windows) <justin.gries@gmail.com>
2020-05-25 07:51:21 -0700

    ada implementation of 'hello, world'

--- hackerrank/30_Days_of_Code/00_Hello_World/ada/src/main.adb
+++ hackerrank/30_Days_of_Code/00_Hello_World/ada/src/main.adb
@@ -1,6 +1,18 @@
+with Ada.Text_IO;
+with Ada.Strings.Unbounded;
+
+--This is a comment
+
 procedure Main is
+package IO renames Ada.Text_IO;
+package SU renames Ada.Strings.Unbounded;
 
 begin
-   --  Insert code here.
-   null;
+   declare
+      input : SU.Unbounded_String;
+   begin
+      input := SU.To_Unbounded_String(IO.Get_Line);
+      IO.Put_Line("Hello, World.");
+      IO.Put_Line(SU.To_String(input));
+   end;
 end Main;
